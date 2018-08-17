8cc2a3715667543ff2cc4af27c2e7e48d65eb1c6
Live session user <martinst@gmail.com>
2018-08-17 21:07:47 +0000

    script to fast install aircrack-ng and dependencies

---
+++ aircrack-install.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+ls -la
+sudo apt-get install autoconf automake libtool shtool openssl libgcrypt11-dev pkg-config ethtool rfkill libnl-3-dev libnl-genl-3-dev libgcrypt11-dev zlib1g-dev libgcrypt20-dev libssl-dev
+wget http://download.aircrack-ng.org/aircrack-ng-1.2.tar.gz
+tar -zxvf aircrack-ng-1.2.tar.gz
+cd aircrack-ng-1.2
+autoreconf -i
+./configure --with-experimental
+make
+sudo make install
+rm -rf aircrack-ng-1.2.tar.gz 
+
+
+
+
