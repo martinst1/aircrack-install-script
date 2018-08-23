085d56ed500574cc8703a50038d366b52400be1a
martinst1 <martinst@gmail.com>
2018-08-24 00:22:16 +0300

    Update aircrack-install.sh

--- aircrack-install.sh
+++ aircrack-install.sh
@@ -2,8 +2,8 @@
 #ls -la
 sudo apt-get install autoconf automake libtool shtool openssl libgcrypt11-dev pkg-config ethtool rfkill libnl-3-dev libnl-genl-3-dev libgcrypt11-dev zlib1g-dev libgcrypt20-dev libssl-dev
 wget http://download.aircrack-ng.org/aircrack-ng-1.2.tar.gz
-tar -zxvf aircrack-ng-1.2.tar.gz
-cd aircrack-ng-1.2
+tar -zxvf aircrack-ng-1.3.tar.gz
+cd aircrack-ng-1.3
 autoreconf -i
 ./configure --with-experimental
 make
